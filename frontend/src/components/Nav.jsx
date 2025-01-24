2aafaf924996f4780d2d9b5b55937519c7d6218d
Ibrahim Ahmed <142117977+Locked-Cloud@users.noreply.github.com>
2025-01-24 22:33:40 +0200

    navbar done for now after this the about as page

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom'
 import "./Nav.css";
 import logo from '../assets/logoAC.png'; // Adjust the path as necessary
 import cartlogo from '../assets/cartlogo.png'; // Ensure this path is correct
+import homelogo from '../assets/home.png'; // Ensure this path is correct
+import aboutlogo from '../assets/aboutas.png'; // Ensure this path is correct
+import userlogo from '../assets/user.png'; // Ensure this path is correct
+import Search from './Search';
 
 export default function Nav() {
   const [click, setClick] = useState(false)
@@ -15,26 +19,7 @@ export default function Nav() {
   return (
     <div className="header">
       <div className="container">
-        <img className="logo" src={logo} alt="logo" />
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/shop">Shop</Link>
-          </li>
-          <li>
-            <Link to="/about">About Us</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact Us</Link>
-          </li>
-          <li>
-            <Link to="/cart">
-              <img className="cart-logo" src={cartlogo} alt="Cart" />
-            </Link>
-          </li>
-        </ul>
+        <Link to="/"><img className="logo" src={logo} alt="logo" /></Link>
         <div className="hamburger" onClick={handleClick}>
           {click ? (
             <FaTimes size={20} style={{ color: "#333" }} />
@@ -42,9 +27,26 @@ export default function Nav() {
             <FaBars size={20} style={{ color: "#333" }} />
           )}
         </div>
+        <Search/>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li>
+            <Link to="/contact">
+              <img className="cart-logo" src={userlogo} alt="user" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <img className="cart-logo" src={cartlogo} alt="Cart" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/about">
+              <img className="cart-logo" src={aboutlogo} alt="about" />
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
 }
-
 
