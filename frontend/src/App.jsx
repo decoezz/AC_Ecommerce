da08893d3a2a70df4def176d3885e85a53bb6b78
Ibrahim Ahmed <142117977+Locked-Cloud@users.noreply.github.com>
2025-01-28 06:50:56 +0200

    profile page done all work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import CreateAccount from './components/CreateAccount';
-import AllUsers from './components/AllUsers';
-import NotFound from './components/NotFound';
-import Login from './components/Login';
-import Main from "./components/Main.jsx"
+import Main from "./components/Main.jsx";
+import "./styles/theme.css";
 
 function App() {
   return (
-    <Main/>
+    <div className="app">
+      <Main />
+    </div>
   );
 }
 
-export default App
-
-
+export default App;
